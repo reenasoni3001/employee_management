5dc0e825b8ad54071d8bc6614888334fb6531254
Reena Soni <reenaprahladsoni@gmail.com>
2022-09-02 20:37:14 +0530

    updated list component

--- src/UI/List.js
+++ src/UI/List.js
@@ -1,8 +1,7 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
@@ -12,26 +11,6 @@ import { deleteUser } from "../features/employeeSlice";
 import { useDispatch } from "react-redux/es/exports";
 import EditEmployee from "../components/EditEmployee";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
-
 const List = ({ employees }) => {
   const dispatch = useDispatch();
 
@@ -39,36 +18,32 @@ const List = ({ employees }) => {
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
-          <TableRow>
-            <StyledTableCell>First Name</StyledTableCell>
-            <StyledTableCell align="right">Last Name</StyledTableCell>
-            <StyledTableCell align="right">DOB</StyledTableCell>
-            <StyledTableCell align="right">Salary</StyledTableCell>
-            <StyledTableCell align="right">Department</StyledTableCell>
-            <StyledTableCell align="right">Actions</StyledTableCell>
+          <TableRow sx={{ backgroundColor: "lightBlue" }}>
+            <TableCell>First Name</TableCell>
+            <TableCell align="right">Last Name</TableCell>
+            <TableCell align="right">DOB</TableCell>
+            <TableCell align="right">Salary</TableCell>
+            <TableCell align="right">Department</TableCell>
+            <TableCell align="right">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {employees.map((employee, i) => (
-            <StyledTableRow key={i}>
-              <StyledTableCell component="th" scope="row">
+            <TableRow key={i}>
+              <TableCell component="th" scope="row">
                 {employee.firstName}
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                {employee.lastName}
-              </StyledTableCell>
-              <StyledTableCell align="right">{employee.age}</StyledTableCell>
-              <StyledTableCell align="right">{`$ ${employee.salary}`}</StyledTableCell>
-              <StyledTableCell align="right">
-                {employee.department}
-              </StyledTableCell>
-              <StyledTableCell align="right">
+              </TableCell>
+              <TableCell align="right">{employee.lastName}</TableCell>
+              <TableCell align="right">{employee.age}</TableCell>
+              <TableCell align="right">{`$ ${employee.salary}`}</TableCell>
+              <TableCell align="right">{employee.department}</TableCell>
+              <TableCell align="right">
                 <EditEmployee employee={employee} />
                 <Button onClick={() => dispatch(deleteUser(employee.id))}>
                   Delete
                 </Button>
-              </StyledTableCell>
-            </StyledTableRow>
+              </TableCell>
+            </TableRow>
           ))}
         </TableBody>
       </Table>
